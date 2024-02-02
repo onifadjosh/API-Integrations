516ef5b8b7c9ffaa29e16a3ea411a7a10282d683
Onifade Joshua <onifadjosh@gmail.com>
2024-02-02 18:25:57 +0100

    API integrations in react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Fetch from './components/Fetch';
+
+function App() {
+  return (
+    <>
+      <Fetch/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Fetch.jsx
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react'
+
+const Fetch = () => {
+    const [photos, Setphotos] = useState([]);
+    const [error, SetError] = useState(null)
+    useEffect(() =>{
+        fetch('https://jsonplaceholder.typicode.com/photos')
+        // .then((res) =>{
+        //     if (!res.ok)
+        // return res.json()})
+        .then((res) =>{
+            if(!res.ok){
+                throw new Error ('failed to fetch');
+            }
+            return res.json()
+        })
+
+        .then((data) =>{
+            console.log(data);
+            Setphotos(data);
+        })
+
+        .catch((error) =>{
+            console.error('Error fetching data:', error);
+            SetError('Failed to fetch data')
+        })
+
+        
+    }, []);
+
+
+
+  return (
+    <div>
+        {error && <p>{error}</p>}
+        {photos.map((photo) =>(
+            <img key={photo.id} src={photo.url} alt={photo.title} width={100} />
+        ))}
+    </div>
+  )
+}
+
+export default Fetch
