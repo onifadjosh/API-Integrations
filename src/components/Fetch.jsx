52785bdbb8fdc1f126e7388f4604b9733b7cac1b
Onifade Joshua <onifadjosh@gmail.com>
2024-02-03 13:01:18 +0100

    Added comment for better understanding

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from 'react'
 
+// using fetch API in react to fetch images
 const Fetch = () => {
     const [photos, Setphotos] = useState([]);
     const [error, SetError] = useState(null)
