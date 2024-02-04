4fc79a05de87952e6b1cb3708e560e40741f42ff
Onifade Joshua <onifadjosh@gmail.com>
2024-02-04 07:33:44 +0100

    Installed dependencies and used better styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import Fetch from './components/Fetch';
+// import Fetch from './components/Fetch';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import MonInt from './components/MonInt';
+import Success from './components/Success';
+
 
 function App() {
   return (
     <>
-      <Fetch/>
+      
+      <Router>
+        
+
+        <Routes>
+          {/* <Fetch/> */}
+          <Route exact path='/' Component={MonInt}/>
+          {/* <MonInt></MonInt> */}
+
+          <Route path='/Success' Component={Success}/>
+          {/* <Success></Success> */}
+        </Routes>
+      </Router>
+
     </>
   )
 }
